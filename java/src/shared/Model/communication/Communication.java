aa2cdf54181d66e8468565f720927c40c7d4db6b
cgrange <cgrange92@gmail.com>
2016-09-29 18:32:38 -0600

    Chat and log deserialization (#72)
    * Communication Deserialization is in. The tests won't work because the model is needed but, as of yet, unimplemented
    
    * some deserialization fixes I also changed the message line so that it holds the name of the player rather than the index of the player
    
    * test fix
    
    * syntax fix
    
    * test fix
    
    * initialize lists

--- java/src/shared/Model/communication/Communication.java
+++ java/src/shared/Model/communication/Communication.java
@@ -1,14 +1,60 @@
 package shared.model.communication;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import shared.model.Model;
 import shared.model.communication.MessageLine;
+import shared.model.players.Player;
 
 public class Communication {
 
 	private List<MessageLine> chatList;
 	private List<MessageLine> logList;
+	
+// ================================== BEGIN CONSTRUCTOR HELPER FUNCTIONS ======================================
+	
+	 private void deserializeChatList(JsonObject chatObj) throws Exception{
+		 chatList = new ArrayList<MessageLine>();
+		 JsonArray linesArray = chatObj.get("lines").getAsJsonArray();
+	   		for(int i = 0; i < linesArray.size(); i++){
+	   			JsonObject lineObject = linesArray.get(i).getAsJsonObject();
+	   			String message = lineObject.get("message").getAsString();
+	   			String source = lineObject.get("source").getAsString();
+	   			MessageLine temp = new MessageLine(source, message);
+	   			chatList.add(temp);
+		   }
+	   }
+	   
+	   private void deserializeLogList(JsonObject logObj) throws Exception{
+		   logList = new ArrayList<MessageLine>();
+		   JsonArray linesArray = logObj.get("lines").getAsJsonArray();
+		   for(int i = 0; i < linesArray.size(); i++){
+			   JsonObject lineObject = linesArray.get(i).getAsJsonObject();
+			   String message = lineObject.get("message").getAsString();
+			   String source = lineObject.get("source").getAsString();
+			   MessageLine temp = new MessageLine(source, message);
+			   logList.add(temp);
+		   }
+	   }
+	   
+// ================================= END CONSTRUCTOR HELPER FUNCTIONS =========================================
+	  
+	   public Communication(String chatJson, String logJson) throws Exception{		   
+		   JsonParser jsonParser = new JsonParser();
+		   
+		   JsonObject chatObj = jsonParser.parse(chatJson).getAsJsonObject();
+		   deserializeChatList(chatObj);
 
-	/**
+		   JsonObject logObj = jsonParser.parse(logJson).getAsJsonObject();
+		   deserializeLogList(logObj);
+	   }
+	   
+	   /**
 	 * Adds a line to the Chat List
 	 * @param line the chat line to add
 	 */
